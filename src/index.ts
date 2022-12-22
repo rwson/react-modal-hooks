eb799dcf43c2097a45e48f4437fdfff0f4f81586
rwson <rwson92@gmail.com>
2022-12-22 09:51:59 +0800

    feat: export basic props

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@ enableAllPlugins();
 export { ModalProvider } from './context';
 export { useModal } from './hooks';
 export { withModals } from './register';
+
+export { ModalBasicProps } from './types';
